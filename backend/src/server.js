b7f1d822719bb21badd640d842be1e3007200018
sarthakbombale <sarthakbomble250@gmail.com>
2025-11-12 23:28:46 +0530

    Clean up ignored files and apply .gitignore

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
-
 import express from "express"
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -9,12 +9,21 @@ import messageRoutes from "./routes/message.route.js";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = (process.env.PORT) || 3000;
 
-app.use("/api/auth",authRoutes)
+app.use("/api/auth", authRoutes)
 
 app.use("/api/message", messageRoutes);
 
+//making production ready 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (_, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => console.log("Server running on port:" + PORT));
