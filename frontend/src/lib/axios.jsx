b28fc930dba6ae963139da9d1eb1a800770c8bc7
sarthakbombale <sarthakbomble250@gmail.com>
2025-11-28 19:33:57 +0530

    axios fix

--- frontend/src/lib/axios.jsx
+++ frontend/src/lib/axios.jsx
@@ -2,4 +2,4 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api", withCredentials: true,
-})
+});
